48d39358d3b39a02682d1233dfa211b835077dee
gabrielnascimento2048 <gabriel.nascimento2048@gmail.com>
2022-02-20 17:50:58 +0000

    add Dependency Inversion and InterfaceSegregation

---
+++ src/main/java/firstProjectSolid/com/solid/tributacao/ReajusteTributavel.java
@@ -0,0 +1,10 @@
+package firstProjectSolid.com.solid.tributacao;
+
+import java.math.BigDecimal;
+
+public interface ReajusteTributavel extends Reajuste {
+
+    BigDecimal valorImpostoDeRenda();
+
+}
+
